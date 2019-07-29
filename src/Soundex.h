f8e363c386c39f7094d0b0dbdf0b995a84a0fdf0
Derek Gui <derek.guiz@gmail.com>
2019-07-30 04:27:39 +1200

    First test failure.

--- src/Soundex.h
+++ src/Soundex.h
@@ -1,7 +1,14 @@
 #ifndef SOUNDEX_H
 #define SOUNDEX_H
+#include <string>
+
 class Soundex
 {
+public:
+    std::string encode(const std::string &word) const
+    {
+        return "";
+    }
 };
 
 #endif
--- test/test.cpp
+++ test/test.cpp
@@ -1,7 +1,9 @@
-#include "gtest/gtest.h"
+#include "gmock/gmock.h"
 #include "Soundex.h"
 
 TEST(SoundexEncoding, TestOneLetterInSigleLtterWord)
 {
     Soundex soundex;
+
+    ASSERT_THAT(soundex.encode("A"), testing::Eq("A"));
 }
