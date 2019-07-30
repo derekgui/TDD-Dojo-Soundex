6e06559467dd59f5126d7c076956fb96443cb61a
Derek Gui <derek.guiz@gmail.com>
2019-07-31 02:05:49 +1200

    Passed test case #4.

--- src/Soundex.h
+++ src/Soundex.h
@@ -14,10 +14,9 @@ public:
 private:
     std::string zeroPad(const std::string &word) const
     {
-        if (word.length() == 1)
-            return head(word) + "000";
+        auto zeros = 4 - word.length();
 
-        return head(word) + encodedDigit(word[1]) + "00";
+        return head(word) + encodedDigits(tail(word)) + std::string(zeros, '0');
     }
 
     std::string head(const std::string &word) const
@@ -25,11 +24,31 @@ private:
         return word.substr(0, 1);
     }
 
+    std::string tail(const std::string &word) const
+    {
+        return word.substr(1, 3);
+    }
+
     std::string encodedDigit(const char letter) const
     {
         const std::unordered_map<char, std::string> encode_map{{'b', "1"}, {'f', "1"}, {'p', "1"}, {'v', "1"}, {'c', "2"}, {'g', "2"}, {'j', "2"}, {'k', "2"}, {'q', "2"}, {'s', "2"}, {'x', "2"}, {'z', "2"}, {'d', "3"}, {'t', "3"}, {'l', "4"}, {'m', "5"}, {'n', "5"}, {'r', "6"}};
 
-        return encode_map.find(letter)->second;
+        auto it = encode_map.find(letter);
+        return (it != encode_map.end()) ? it->second : "0";
+    }
+
+    std::string encodedDigits(const std::string &word) const
+    {
+        std::string result;
+
+        if (word.length() > 0)
+        {
+            for (auto letter : word)
+                result += encodedDigit(letter);
+            return result;
+        }
+
+        return "";
     }
 };
 
